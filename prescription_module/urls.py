2496d6f0c632f89cfa4d67912936baf41a70d58e
Ata <Ata4FM@gmail.com>
2024-12-08 23:42:49 +0330

    Changed Some Componnent Color

--- prescription_module/urls.py
+++ prescription_module/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.PrescriptionView.as_view(), name='prescription'),
     path('add-prescription', views.add_medicine_to_prescription, name='add-prescription-to-prescription'),
-    path('remove-prescription-detailremove-prescription-detail',views.remove_prescription_basket_detail,name='remove-prescription-basket-detail'),
+    path('remove-prescription-detail',views.remove_prescription_basket_detail,name='remove-prescription-basket-detail'),
     path('change-prescription-detail-count',views.change_order_detail_count,name='change-prescription-detail-count'),
 ]
